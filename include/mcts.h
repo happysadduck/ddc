852f91688d5b58b3cf4c1a64b58ef7c8aac82594
happysadduck <happysadduck@noreply.gitcode.com>
2026-03-15 10:31:28 +0800

    0.1pre2.1

--- include/mcts.h
+++ include/mcts.h
@@ -1,4 +1,19 @@
 #ifndef MCTS_H
 #define MCTS_H
 
+typedef struct MCTSNode
+{
+    void *action;
+    void *untried_actions;
+    void *state;
+    struct MCTSNode *children;
+    struct MCTSNode *parent;
+    struct MCTSNode *next;
+
+    int visit_cnt;
+    float total_value;
+    float prior_prob;
+    int player_id;
+} MCTSNode;
+
 #endif
